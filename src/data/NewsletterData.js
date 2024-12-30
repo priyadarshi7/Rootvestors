f8eee00c6b6111679db621d7255fe193abdfbcb2
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-12-30 12:49:11 +0530

    dec30 newlaetter

--- src/data/NewsletterData.js
+++ src/data/NewsletterData.js
@@ -8,16 +8,6 @@ import Rootwise_30dec from "../assets/pdf/Rootwise_30December.pdf"
 
 const NewsletterData = [
     {
-        id:1,
-        date:"December 16, 2024",
-        edition:1,
-        volume:1,
-        coverImage:img1,
-        content: "A practical guide to financial literacy, covering budgeting, saving, investing, and key concepts to master money management effectively.",
-        pdfSrc:Rootwise_16dec,
-        route:"/newsletter/view?id=1&edition=1&volume=1"
-    },
-    {
         id:2,
         date:"December 30, 2024",
         edition:1,
@@ -26,6 +16,16 @@ const NewsletterData = [
         content: "Your go-to guide for mastering money—featuring investing hacks, compound interest secrets, and the mindset to level up your finances!",
         pdfSrc:Rootwise_30dec,
         route:"/newsletter/view?id=1&edition=1&volume=2"
+    },
+    {
+        id:1,
+        date:"December 16, 2024",
+        edition:1,
+        volume:1,
+        coverImage:img1,
+        content: "A practical guide to financial literacy, covering budgeting, saving, investing, and key concepts to master money management effectively.",
+        pdfSrc:Rootwise_16dec,
+        route:"/newsletter/view?id=1&edition=1&volume=1"
     }
 ]
 
